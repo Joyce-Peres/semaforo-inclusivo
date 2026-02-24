2b806fb196010657c42c2cb0ae67f4595cbf6bb2
Joyce <joycecostaperes@gmail.com>
2026-02-24 10:48:43 -0300

    feat: adicionando mudanças de codigo

--- main.ino
+++ main.ino
@@ -1,3 +1,6 @@
+// Semáforo inclusivo com display 7 segmentos (CÁTODO COMUM)
+// Carros automáticos + botão adianta para pedestre
+ 
 // Luzes dos carros
 int carRed = 12;
 int carYellow = 11;
@@ -7,7 +10,7 @@ int carGreen = 10;
 int pedRed = 8;
 int pedGreen = 9;
  
-// Buzzer inclusivo
+// Buzzer
 int buzzer = 7;
  
 // Botão
@@ -22,21 +25,29 @@ int segE = A0;
 int segF = A1;
 int segG = A2;
  
-unsigned long changeTime = 0;
+// Tempos automáticos
+unsigned long greenTime = 10000;
+unsigned long yellowTime = 2000;
+unsigned long lastChange = 0;
  
-// Tabela para CÁTODO COMUM (HIGH acende)
+int state = 0;
+// 0 = Verde carros
+// 1 = Amarelo carros
+// 2 = Vermelho carros (pedestre aberto)
+ 
+ 
+// Tabela CÁTODO COMUM (HIGH acende)
 int digits[10][7] = {
-  // A  B  C  D  E  F  G
-  {1, 1, 1, 1, 1, 1, 0}, // 0
-  {0, 1, 1, 0, 0, 0, 0}, // 1
-  {1, 1, 0, 1, 1, 0, 1}, // 2
-  {1, 1, 1, 1, 0, 0, 1}, // 3
-  {0, 1, 1, 0, 0, 1, 1}, // 4
-  {1, 0, 1, 1, 0, 1, 1}, // 5
-  {1, 0, 1, 1, 1, 1, 1}, // 6
-  {1, 1, 1, 0, 0, 0, 0}, // 7
-  {1, 1, 1, 1, 1, 1, 1}, // 8
-  {1, 1, 1, 1, 0, 1, 1}  // 9
+  {1,1,1,1,1,1,0}, //0
+  {0,1,1,0,0,0,0}, //1
+  {1,1,0,1,1,0,1}, //2
+  {1,1,1,1,0,0,1}, //3
+  {0,1,1,0,0,1,1}, //4
+  {1,0,1,1,0,1,1}, //5
+  {1,0,1,1,1,1,1}, //6
+  {1,1,1,0,0,0,0}, //7
+  {1,1,1,1,1,1,1}, //8
+  {1,1,1,1,0,1,1}  //9
 };
  
 void setup() {
@@ -44,10 +55,8 @@ void setup() {
   pinMode(carRed, OUTPUT);
   pinMode(carYellow, OUTPUT);
   pinMode(carGreen, OUTPUT);
- 
   pinMode(pedRed, OUTPUT);
   pinMode(pedGreen, OUTPUT);
- 
   pinMode(buzzer, OUTPUT);
   pinMode(button, INPUT);
  
@@ -61,44 +70,65 @@ void setup() {
  
   // Estado inicial
   digitalWrite(carGreen, HIGH);
-  digitalWrite(carYellow, LOW);
-  digitalWrite(carRed, LOW);
- 
   digitalWrite(pedRed, HIGH);
-  digitalWrite(pedGreen, LOW);
  
-  clearDisplay();
+  lastChange = millis();
 }
  
 void loop() {
-  int state = digitalRead(button);
  
-  if (state == HIGH && (millis() - changeTime) > 5000) {
-    changeLights();
+  unsigned long currentMillis = millis();
+ 
+  // BOTÃO adianta o verde
+  if (digitalRead(button) == HIGH && state == 0) {
+    state = 1; // vai para amarelo
+    lastChange = currentMillis;
+    digitalWrite(carGreen, LOW);
+    digitalWrite(carYellow, HIGH);
   }
  
-  clearDisplay();
+  switch(state) {
+ 
+    case 0: // Verde carros
+      if (currentMillis - lastChange >= greenTime) {
+        state = 1;
+        lastChange = currentMillis;
+        digitalWrite(carGreen, LOW);
+        digitalWrite(carYellow, HIGH);
+      }
+    break;
+ 
+    case 1: // Amarelo carros
+      if (currentMillis - lastChange >= yellowTime) {
+        state = 2;
+        lastChange = currentMillis;
+ 
+        digitalWrite(carYellow, LOW);
+        digitalWrite(carRed, HIGH);
+ 
+        openPedestrian();
+      }
+    break;
+ 
+    case 2:
+      // Espera a função openPedestrian terminar
+      state = 0;
+      lastChange = millis();
+      digitalWrite(carRed, LOW);
+      digitalWrite(carGreen, HIGH);
+    break;
+  }
+ 
+  if(state != 2){
+    clearDisplay();
+  }
 }
  
-void changeLights() {
+void openPedestrian() {
  
-  // Verde desliga direto
-  digitalWrite(carGreen, LOW);
- 
-  // Amarelo (3s)
-  digitalWrite(carYellow, HIGH);
-  delay(3000);
- 
-  // Vermelho
-  digitalWrite(carYellow, LOW);
-  digitalWrite(carRed, HIGH);
-  delay(1000);
- 
-  // Pedestre verde
   digitalWrite(pedRed, LOW);
   digitalWrite(pedGreen, HIGH);
  
-  // Contagem regressiva 5 → 1
   for (int i = 5; i > 0; i--) {
     showDigit(i);
  
@@ -121,11 +151,6 @@ void changeLights() {
   digitalWrite(pedRed, HIGH);
   noTone(buzzer);
   clearDisplay();
- 
-  digitalWrite(carRed, LOW);
-  digitalWrite(carGreen, HIGH);
- 
-  changeTime = millis();
 }
  
 void showDigit(int num) {
